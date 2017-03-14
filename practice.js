f9d545a0b7fbaa0accf5ca83146e0c8c68cc744b
Atmanand Citigori <atmanand.citigori@gmail.com>
2017-03-14 16:13:02 -0400

    Create practice.js

---
+++ practice.js
@@ -0,0 +1,8 @@
+How to create a Range function:
+function fRange(Start, end){ 
+  myArr.length = 0;    
+    for(var i=start;i<=end;i++){     
+      myArr.push(i);    
+    }    
+  return myArr;
+}
